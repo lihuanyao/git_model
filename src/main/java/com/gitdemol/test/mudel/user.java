33a167c414ce6cac6dfb32ecbc141c2b0d1e6b9a
寂夜寒轩 <1551318033@qq.com>
2019-12-16 15:39:51 +0800

    添加实类

---
+++ src/main/java/com/gitdemol/test/mudel/user.java
@@ -0,0 +1,18 @@
+package com.gitdemol.test.mudel;
+
+import lombok.Data;
+
+/**
+ * 描述: 【】
+ *
+ * @author: LiHY
+ * @email: lihy@zssinyi.com
+ * @date: 2019/12/16 15:14
+ * @version: V1.0
+ */
+@Data
+public class user {
+    private String userName;
+    private Integer age;
+}
+
